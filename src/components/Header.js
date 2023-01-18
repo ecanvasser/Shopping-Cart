e42d29ce8e2965c4080c12f06ef76e755e1c79fe
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-18 01:11:09 -0800

    Add tailwind styling to Main.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import { AiOutlineShoppingCart as Cart} from "react-icons/ai";
-import styles from "../index.css"
+import "../index.css"
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center py-6 px-10">
       <div id="title">Espresso Emporium</div>
-      <ul id="nav-links" className="flex gap-5">
+      <ul id="nav-links" className="flex gap-10">
         <li>Home</li>
         <li>Shop</li>
         <li id="cart-icon"><Cart/></li>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,11 @@
-import "../styles/Main.css";
+import "../index.css";
 import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
-    <main id="home-main">
-      <div id="main-text">ESPRESSO REDEFINED</div>
-      <Link to="/store" id="cta-btn">
+    <main id="home-main" className="bg-hero bg-center flex flex-col gap-5 justify-center items-center text-white">
+      <div id="main-text" className="text-6xl font-extrabold">ESPRESSO REDEFINED</div>
+      <Link to="/store" id="cta-btn" className="border rounded-md px-2 py-2">
         Shop Espresso
       </Link>
     </main>
