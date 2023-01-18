8594555ee3615e85e86c5d33647499426df563fd
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-17 21:14:18 -0800

    Create Store.js & Store.css

---
+++ src/components/Store.js
@@ -0,0 +1,8 @@
+import "../styles/Store.css";
+import Header from "./Header";
+
+const Store = () => {
+  return <Header />;
+};
+
+export default Store;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import Home from './components/Home.js'
+import Home from './components/Home.js';
+import Store from './components/Store'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" />
+      <Route path="/" index element={<Home />} />
+      <Route path="/store" element={<Store />}/>
       <Route path="/cart" />
     </Routes>
   </BrowserRouter>
