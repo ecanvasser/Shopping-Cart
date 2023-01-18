8701e532ed716f12ed30e7f3fbf2e04cba6f52a6
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-18 01:14:34 -0800

    Update routes on header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { AiOutlineShoppingCart as Cart} from "react-icons/ai";
+import { Link } from "react-router-dom";
 import "../index.css"
 
 const Header = () => {
@@ -6,8 +7,8 @@ const Header = () => {
     <header className="flex justify-between items-center py-6 px-10">
       <div id="title">Espresso Emporium</div>
       <ul id="nav-links" className="flex gap-10">
-        <li>Home</li>
-        <li>Shop</li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/store">Shop</Link></li>
         <li id="cart-icon"><Cart/></li>
       </ul>
     </header>
