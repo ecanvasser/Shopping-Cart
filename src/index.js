6e3c2780f1858f10a9c7abe0578c794aec6e9cd6
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-17 20:28:40 -0800

    Removed App.js & App.css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import App from "./components/App.js";
+import Home from './components/Home.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Home />} />
       <Route path="/store" />
       <Route path="/cart" />
     </Routes>
