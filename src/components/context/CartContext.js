a1fe8ff375c6b549a637b5f16c27ffdc099904cb
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-24 15:00:30 -0800

    Finish first rendition of addItems function

--- src/components/context/CartContext.js
+++ src/components/context/CartContext.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { createContext } from "react";
 import StoreContext from "./StoreContext";
 
@@ -8,22 +8,61 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const { products } = useContext(StoreContext);
 
+  //Problem is how this is saving array. Need to check below in addItems
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem("cartItems"))) {
+      setCartItems(JSON.parse(localStorage.getItem("cartItems")));
+    }
+
+    // if (JSON.parse(localStorage.getItem("cartItems"))) {
+    //   if (Array.isArray(JSON.parse(localStorage.getItem("cartItems"))[0])) {
+    //     setCartItems(JSON.parse(localStorage.getItem("cartItems"))[0]);
+    //   } else {
+    //     setCartItems([JSON.parse(localStorage.getItem("cartItems"))[0]]);
+    //   }
+    // }
+  }, []);
+
   const addItems = (e) => {
+    const clone = [...cartItems].concat(products[e.target.id]);
+    localStorage.setItem("cartItems", JSON.stringify(clone));
+    setCartItems(cartItems.concat(products[e.target.id]));
+
     // If cart already has items, find match and update quantity.
     // Else, add new item to cartItems
-    if (cartItems.length > 0) {
-      const updatedArray = cartItems.map((item) => {
-        if (item.name === products[e.target.id].name) {
-          return {
-            ...item,
-            quantity: item.quantity + products[e.target.id].quantity,
-          };
-        }
-      });
-      setCartItems(updatedArray)
-    } else {
-      setCartItems(cartItems.concat(products[e.target.id]));
-    }
+    // if (cartItems) {
+    //   const updatedArray = cartItems.map((item) => {
+    //     if (item.name === products[e.target.id].name) {
+    //       console.log("item.name", item.name);
+    //       console.log("products[e.target.id].name", products[e.target.id].name);
+
+    //   let clone = cartItems.filter((i) => {
+    //     return i.name === item.name;
+    //   });
+
+    // let amount = products[e.target.id].quantity;
+    //   let amount = 6
+    //   console.log("amount", amount);
+    //   let copy = [...cartItems];
+    //   copy[index] = {
+    //     ...item,
+    //     quantity: item.quantity + amount,
+    //   };
+    //   copy.splice(index, 1, copy[index])
+    //       return [...cartItems,  products[e.target.id].quantity = amount ];
+    //     } else {
+    //       return [...cartItems, products[e.target.id]];
+    //     }
+    //   });
+    //   localStorage.setItem("cartItems", JSON.stringify(updatedArray));
+    //   setCartItems(JSON.parse(localStorage.getItem("cartItems"))[0]);
+    // } else {
+    //   localStorage.setItem(
+    //     "cartItems",
+    //     JSON.stringify([products[e.target.id]])
+    //   );
+    //   setCartItems([products[e.target.id]]);
+    // }
   };
 
   return (
