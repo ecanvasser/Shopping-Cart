38924d0f01291fe8e97d5c1f9092544d579e461d
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-24 15:16:48 -0800

    Fix route typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import "./styles/index.css";
 import Home from "./components/Home.js";
 import Store from "./components/Store";
 import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,7 +15,7 @@ root.render(
       <Route index element={<Home />} />
       <Route path="/store" exact element={<Store />} />
       <Route path="/store/:id" element={<ProductDetails />}/>
-      <Route path="/cart" />
+      <Route path="/cart" element={<Cart />}/>
     </Routes>
   </BrowserRouter>
 );
